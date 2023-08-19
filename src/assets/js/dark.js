74b430f327669af4c9c3d0ed3e657014885df98f
theantidoak <tcdoak@gmail.com>
2023-08-19 09:58:46 +0200

    inital commit

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -1,8 +1,13 @@
-//
-//    The Dark Mode System
-//
+document.addEventListener('DOMContentLoaded', () => {
 
-// helper functions to toggle dark mode
+  detectColorScheme();
+
+  document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+    localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
+  });
+
+})
+
 function enableDarkMode() {
 	document.body.classList.add('dark-mode');
 	localStorage.setItem('theme', 'dark');
@@ -12,29 +17,14 @@ function disableDarkMode() {
 	localStorage.setItem('theme', 'light');
 }
 
-// determines a new users dark mode preferences
 function detectColorScheme() {
-	// default to the light theme
 	let theme = 'light';
 
-	// check localStorage for a saved 'theme' variable. if it's there, the user has visited before, so apply the necessary theme choices
 	if (localStorage.getItem('theme')) {
 		theme = localStorage.getItem('theme');
-	}
-	// if it's not there, check to see if the user has applied dark mode preferences themselves in the browser
-	else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 		theme = 'dark';
 	}
 
-	// if there is no preference set, the default of light will be used. apply accordingly
 	theme === 'dark' ? enableDarkMode() : disableDarkMode();
 }
-
-// run on page load
-detectColorScheme();
-
-// add event listener to the dark mode button toggle
-document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-	// on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
-	localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
-});
