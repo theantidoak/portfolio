efe8cc87ac325548fbab07239f0202bf1097b70e
theantidoak <tcdoak@gmail.com>
2023-08-20 14:45:33 +0200

    start on home page

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -1,30 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  detectColorScheme();
+//   detectColorScheme();
 
-  document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-    localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
-  });
+//   document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+//     localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
+//   });
 
-})
+// })
 
-function enableDarkMode() {
-	document.body.classList.add('dark-mode');
-	localStorage.setItem('theme', 'dark');
-}
-function disableDarkMode() {
-	document.body.classList.remove('dark-mode');
-	localStorage.setItem('theme', 'light');
-}
+// function enableDarkMode() {
+// 	document.body.classList.add('dark-mode');
+// 	localStorage.setItem('theme', 'dark');
+// }
+// function disableDarkMode() {
+// 	document.body.classList.remove('dark-mode');
+// 	localStorage.setItem('theme', 'light');
+// }
 
-function detectColorScheme() {
-	let theme = 'light';
+// function detectColorScheme() {
+// 	let theme = 'light';
 
-	if (localStorage.getItem('theme')) {
-		theme = localStorage.getItem('theme');
-	} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		theme = 'dark';
-	}
+// 	if (localStorage.getItem('theme')) {
+// 		theme = localStorage.getItem('theme');
+// 	} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+// 		theme = 'dark';
+// 	}
 
-	theme === 'dark' ? enableDarkMode() : disableDarkMode();
-}
+// 	theme === 'dark' ? enableDarkMode() : disableDarkMode();
+// }
--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,36 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  var CSbody = document.querySelector('body');
-  const CSnavbarMenu = document.querySelector('#cs-navigation');
-  const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+//   var CSbody = document.querySelector('body');
+//   const CSnavbarMenu = document.querySelector('#cs-navigation');
+//   const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
 
-  CShamburgerMenu.addEventListener('click', function () {
-    CShamburgerMenu.classList.toggle('cs-active');
-    CSnavbarMenu.classList.toggle('cs-active');
-    CSbody.classList.toggle('cs-open');
+//   CShamburgerMenu.addEventListener('click', function () {
+//     CShamburgerMenu.classList.toggle('cs-active');
+//     CSnavbarMenu.classList.toggle('cs-active');
+//     CSbody.classList.toggle('cs-open');
 
-    ariaExpanded();
-  });
+//     ariaExpanded();
+//   });
 
-  const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
-  for (const item of dropDowns) {
-    const onClick = () => {
-      item.classList.toggle('cs-active');
-    };
-    item.addEventListener('click', onClick);
-  }
+//   const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+//   for (const item of dropDowns) {
+//     const onClick = () => {
+//       item.classList.toggle('cs-active');
+//     };
+//     item.addEventListener('click', onClick);
+//   }
 
-})
+// })
 
 
 
-function ariaExpanded() {
-	const csUL = document.querySelector('#cs-expanded');
-	const csExpanded = csUL.getAttribute('aria-expanded');
+// function ariaExpanded() {
+// 	const csUL = document.querySelector('#cs-expanded');
+// 	const csExpanded = csUL.getAttribute('aria-expanded');
 
-	if (csExpanded === 'false') {
-		csUL.setAttribute('aria-expanded', 'true');
-	} else {
-		csUL.setAttribute('aria-expanded', 'false');
-	}
-}
+// 	if (csExpanded === 'false') {
+// 		csUL.setAttribute('aria-expanded', 'true');
+// 	} else {
+// 		csUL.setAttribute('aria-expanded', 'false');
+// 	}
+// }
